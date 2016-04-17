c6fb4096d63bbef98f903907b28b2be8e0f91cec
Brian Pareja <benjytj2@gmail.com>
2016-04-16 22:56:24 -0500

    Avance en peliculas

---
+++ mostrarpeliculas.php
@@ -0,0 +1,116 @@
+<?php require_once("header.php"); 
+require_once("header.php"); 
+require_once("funciones.php");
+$xc = conectar();
+$sql="SELECT * FROM dbcine.perfil";
+$res=mysqli_query($xc,$sql);
+
+desconectar($xc);
+
+?>
+        <div id="page-wrapper" >
+            <div id="page-inner">
+                <div class="row">
+                    <div class="col-md-12">
+                     <h2>Tabla de Peliculas</h2>   
+                        <h5>Aqui podrá agregar y seleccionar las peliculas a editar y eliminar</h5>
+                       
+                    </div>
+                </div>
+                 <!-- /. ROW  -->
+                 <hr />
+               
+            <div class="row">
+                <div class="col-md-12">
+                    <!-- Advanced Tables -->
+                    <div class="panel panel-default">
+                        <div class="panel-heading">
+                            Peliculas
+                        </div>
+                        <div class="panel-body">
+                            <div class="table-responsive">
+                                <table class="table table-striped table-bordered table-hover" id="dataTables-example">
+                                    <thead>
+                                        <tr>
+                                            <th>Nombre</th>
+                                            <th>Duración</th>
+                                            <th>Fecha de Estreno</th>
+                                            <th>Formato</th>
+                                            <th># Vistos</th>
+                                        </tr>
+                                    </thead>
+                                    <tbody>
+
+
+
+  <?php
+    while ($row=mysqli_fetch_array($res))
+    {
+      echo"<tr> <td>";
+        echo $row['nombre_Peli'];
+      echo"</td>";
+      echo"<td>";
+        echo $row['duracion_Peli'];
+      echo"</td>";
+      echo"<td>";
+        echo $row['estreno_Peli'];
+      echo"</td>";
+      echo"<td>";
+        echo $row['formato_Peli'];
+      echo"</td>";
+       echo"<td>";
+        echo $row['vistos_Peli'];
+      echo"</td>";
+      ?>
+      <button class="btn btn-primary"><i class="fa fa-edit "></i> Edit</button>
+    <?php 
+            echo"</td>";
+     echo"<td>"; ?>
+      <button class="btn btn-danger"><i class="fa fa-pencil"></i> Delete</button>
+    <?php 
+            echo"</td>";
+       }
+     ?>
+
+                                     </tbody>
+                                </table>
+                            </div>
+                            
+                        </div>
+                    </div>
+                    <!--End Advanced Tables -->
+                </div>
+            </div>   
+                <!-- /. ROW  -->
+           
+                  <!--  hasta aquiiiiiiiiiiiiiiii  -->
+                <!-- /. ROW  -->
+        </div>
+               
+    </div>
+             <!-- /. PAGE INNER  -->
+            </div>
+         <!-- /. PAGE WRAPPER  -->
+     <!-- /. WRAPPER  -->
+    <!-- SCRIPTS -AT THE BOTOM TO REDUCE THE LOAD TIME-->
+    <!-- JQUERY SCRIPTS -->
+    <script src="assets/js/jquery-1.10.2.js"></script>
+      <!-- BOOTSTRAP SCRIPTS -->
+    <script src="assets/js/bootstrap.min.js"></script>
+    <!-- METISMENU SCRIPTS -->
+    <script src="assets/js/jquery.metisMenu.js"></script>
+     <!-- DATA TABLE SCRIPTS -->
+    <script src="assets/js/dataTables/jquery.dataTables.js"></script>
+    <script src="assets/js/dataTables/dataTables.bootstrap.js"></script>
+        <script>
+            $(document).ready(function () {
+                $('#dataTables-example').dataTable();
+            });
+    </script>
+         <!-- CUSTOM SCRIPTS -->
+    <script src="assets/js/custom.js"></script>
+    
+   
+
+
+
